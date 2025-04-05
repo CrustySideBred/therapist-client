063bcf395bf01b8bcebc29204ed25aafd2a09cd1
Raja Mrabti <rajamrabti15@gmail.com>
2025-04-05 19:34:32 +0100

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './Home.css';
+
+// Home Component (Music Player UI)
+const Home = () => {
+
+}
+
+export default Home;
