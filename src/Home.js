9e1b83f0bc656f7fd0a1779a40d01cc4294c3631
Raja Mrabti <rajamrabti15@gmail.com>
2025-04-06 23:06:51 +0100

    created basics of homepage

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './Home.css';
+import Client from './pages/Client';
+import Session from './pages/Session';
+import Therapist from './pages/Therapist';
 
-// Home Component (Music Player UI)
 const Home = () => {
-
+    return (
+        <div className="Home-container">
+            <h1>Therapist-Client-Management-System</h1>
+        </div>
+    );
 }
 
-export default Home;
+function App() {
+    return (
+        <Router>
+            <nav className="navigation-bar">
+                <Link to="/">Home</Link>
+                <Link to="/Session">Session</Link>
+                <Link to="/Client">Client</Link>
+                <Link to="/Therapist">Therapist</Link>
+            </nav>
+            
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Session" element={<Session />} />
+                <Route path="/Client" element={<Client />} />
+                <Route path="/Therapist" element={<Therapist />} />
+            </Routes>
+        </Router>
+    );
+}
+
+
+export default App;
