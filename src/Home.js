697e089b706a38d0d4cda2b4461a44009d352a5e
Raja Mrabti <rajamrabti15@gmail.com>
2025-04-07 22:44:45 +0100

    updated home page display

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './Home.css';
+import './Home.css'; // Changed from Home.css to App.css since this is your main component
 import Client from './pages/Client';
 import Session from './pages/Session';
 import Therapist from './pages/Therapist';
 
 const Home = () => {
     return (
-        <div className="Home-container">
-            <h1>Therapist-Client-Management-System</h1>
+        <div className="home-container">
+            <h1>Welcome to the Home page!</h1>
         </div>
     );
 }
@@ -16,22 +16,34 @@ const Home = () => {
 function App() {
     return (
         <Router>
-            <nav className="navigation-bar">
-                <Link to="/">Home</Link>
-                <Link to="/Session">Session</Link>
-                <Link to="/Client">Client</Link>
-                <Link to="/Therapist">Therapist</Link>
-            </nav>
+            <header className="app-header">
+                <nav className="navigation-bar">
+                    <div className="nav-left">
+                        <Link to="/">Home</Link>
+                        <Link to="/Client">Client</Link>
+                    </div>
+                    <div className="nav-center">
+                        <Link to="/" className="home-link">
+                            <h1 className="app-title">Therapist-Client-Management-System</h1>
+                        </Link>
+                    </div>
+                    <div className="nav-right">
+                        <Link to="/Therapist">Therapist</Link>
+                        <Link to="/Session">Session</Link>
+                    </div>
+                </nav>
+            </header>
             
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Session" element={<Session />} />
-                <Route path="/Client" element={<Client />} />
-                <Route path="/Therapist" element={<Therapist />} />
-            </Routes>
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Session" element={<Session />} />
+                    <Route path="/Client" element={<Client />} />
+                    <Route path="/Therapist" element={<Therapist />} />
+                </Routes>
+            </main>
         </Router>
     );
 }
-
 
 export default App;
